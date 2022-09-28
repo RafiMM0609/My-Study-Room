d01a8d5ecb4b9b7248576f84a59ebc19e9fc4b58
Rafi Muhammad Mahrus <75168126+RafiMM0609@users.noreply.github.com>
2022-09-29 04:07:07 +0700

    Add files via upload

---
+++ my-study-room/dashboard/forms.py
@@ -0,0 +1,26 @@
+from dataclasses import fields
+from datetime import date
+from django import forms 
+from . models import *
+
+class NotesForm(forms.ModelForm):
+    class Meta :
+        model = Notes
+        fields = ['title','description']
+
+class DateInput(forms.DateInput):
+     input_type =  'date'
+
+class HomeworkForm(forms.ModelForm):
+    class Meta :
+        model = Homework
+        widgets = { 'due':DateInput}
+        fields = ['subject', 'title', 'description', 'due', 'is_finished']
+
+class DashboardForm(forms.Form):
+    text = forms.CharField(max_length=100, label="enter your seacrh : ")
+
+class TodoForm(forms.ModelForm):
+    class Meta :
+        model = Todo
+        fields = ['title','is_finished']
